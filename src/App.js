ce5acf1f45fb8e8ccd89ce794209fa686c92fec9
José Luis Valdiviezo Peña <luisj.valdiviezo20@gmail.com>
2021-01-12 11:57:15 -0500

    pages complete render

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Header from './Components/Header'
 import AboutMe from './pages/AboutMe'
 import DashBoard from './pages/DashBoard'
+import Projects from './pages/Projects'
+import Contacts from './pages/Contacts'
 import './App.css';
 
 function App() {
@@ -13,7 +15,8 @@ function App() {
       <Switch>
       <Route exact path="/" component={DashBoard}/>
       <Route exact path="/aboutme" component={AboutMe}/>
-
+      <Route exact path="/projects" component={Projects}/>
+      <Route exact path="/contacts" component={Contacts}/>
     </Switch>
     </div>
     </BrowserRouter>
---
+++ src/Components/Project.js
@@ -0,0 +1,20 @@
+function Project(){
+  return(
+    <div class="projects__project">
+          <div class="project__wrapper">
+            <img class="project__wrapper_image" src="./asset/r-p-s.png" alt="rock-papper-scissors"/>
+          </div>
+          <div class="project__info">
+            <p class="project__info_text">
+              This is a description for the project
+              This is a description for the project
+              This is a description for the project
+              This is a description for the project
+            </p>
+            <button class="project__info_view">Lorem Ipsum</button>
+          </div>
+    </div>
+  )
+}
+
+export default Project
